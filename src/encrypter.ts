352cfe3296aab9b4b93517d6631aa441d5381472
sean nicholas <sn@netvise.de>
2019-04-23 19:05:09 +0200

    encrypter returns promise

--- src/encrypter.ts
+++ src/encrypter.ts
@@ -2,14 +2,16 @@ import { encrypt } from './crypto'
 import { createInterface } from 'readline'
 
 export function encrypter(encryptionKey: string) {
-  const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
+  return new Promise(res => {
+    const rl = createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
 
-  rl.question('What to encrypt? ', (answer) => {
-    console.log(encrypt(answer, encryptionKey))
-
-    rl.close()
+    rl.question('What to encrypt? ', (answer) => {
+      console.log(encrypt(answer, encryptionKey))
+      rl.close()
+      res()
+    })
   })
 }
